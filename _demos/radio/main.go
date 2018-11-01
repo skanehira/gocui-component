0790ccbc8d8608885bbe2dd057b903bf59005752
skanehira <sho19921005@gmail.com>
2018-11-01 18:52:53 +0900

    - Fix button demo - Add demo gif

--- _demos/radio/main.go
+++ _demos/radio/main.go
@@ -24,12 +24,14 @@ func main() {
 	}
 
 	demo := &demo{}
-	demo.radios = append(demo.radios, component.NewRadio(gui, "Go", 0, 0).AddHandler(gocui.KeyTab, demo.changeRadio))
-	demo.radios = append(demo.radios, component.NewRadio(gui, "PHP", 0, 1).AddHandler(gocui.KeyTab, demo.changeRadio))
-	demo.radios = append(demo.radios, component.NewRadio(gui, "Java", 0, 2).AddHandler(gocui.KeyTab, demo.changeRadio))
+	demo.radios = append(demo.radios, component.NewRadio(gui, "Go", 0, 0))
+	demo.radios = append(demo.radios, component.NewRadio(gui, "PHP", 0, 1))
+	demo.radios = append(demo.radios, component.NewRadio(gui, "Java", 0, 2))
 
 	for _, r := range demo.radios {
-		r.Draw()
+		r.AddHandler(gocui.KeyTab, demo.changeRadio).
+			AddHandler(gocui.KeyEnter, demo.checkRadio).
+			Draw()
 	}
 
 	demo.radios[0].Focus()
@@ -47,5 +49,15 @@ func (d *demo) changeRadio(g *gocui.Gui, v *gocui.View) error {
 	d.radios[d.active].UnFocus()
 	d.active = (d.active + 1) % len(d.radios)
 	d.radios[d.active].Focus()
+	return nil
+}
+
+func (d *demo) checkRadio(g *gocui.Gui, v *gocui.View) error {
+	for _, r := range d.radios {
+		v, _ := g.View(r.GetLabel())
+		r.UnCheck(g, v)
+	}
+
+	d.radios[d.active].Check(g, v)
 	return nil
 }
